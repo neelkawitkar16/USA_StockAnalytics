39492444d1206d669d1f49bfdc6ba259574f6ff4
Neel Kawitkar <neel.kawitkar16@gmail.com>
2025-03-13 14:22:10 -0400

    Java Session4: 1. Arrays and loops and control statements 2. Integer, Character, Float, String Arrays. 3. Iterating through an integer array and sorting using for loop 4. for each loop and if cases

---
+++ com/eureka/stockAnalysis/inheritance/Test.java
@@ -0,0 +1,31 @@
+package com.eureka.stockAnalysis.inheritance;
+
+public class Test {
+    private String test_1;
+    private String test_2;
+    public String test_3;
+
+    public String getTest_1() {
+        return test_1;
+    }
+
+    public void setTest_1(String test_1) {
+        this.test_1 = test_1;
+    }
+
+    public String getTest_2() {
+        return test_2;
+    }
+
+    public void setTest_2(String test_2) {
+        this.test_2 = test_2;
+    }
+
+    public String getTest_3() {
+        return test_3;
+    }
+
+    public void setTest_3(String test_3) {
+        this.test_3 = test_3;
+    }
+}
---
+++ com/eureka/stockAnalysis/inheritance/TestPlayground.java
@@ -0,0 +1,8 @@
+package com.eureka.stockAnalysis.inheritance;
+
+public class TestPlayground {
+    public static void main(String[] args) {
+        Test temp = new Test();
+        System.out.println(temp.getTest_1());
+    }
+}
